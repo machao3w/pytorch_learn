a9f09fefcf6b93618feb2c43be286b2fe3b8616f
machao32 <248402636@qq.com>
2023-02-28 23:13:04 +0800

    test

--- Test3.py
+++ Test3.py
@@ -38,5 +38,38 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
             ax.set_title(titles[i])
     return axes
 
+
 x, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
-show_images(x.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
+# show_images(x.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
+
+batch_size = 256
+
+
+def get_dataloader_workers():
+    return 4
+
+
+if __name__ == '__main__':
+    # 由于使用了多线程 所以要放入main函数里运行
+    train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers())
+    timer = d2l.Timer()
+    for x, y in train_iter:
+        # print(x)
+        # print(y)
+        continue
+    print(f'{timer.stop():.2f} sec')
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """ 下载数据集，加载到内存"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        # 将图片变大
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root="./data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="./data", train=False, transform=trans, download=True)
+    return (
+        data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers()),
+        data.DataLoader(mnist_test, batch_size, shuffle=True, num_workers=get_dataloader_workers())
+    )
